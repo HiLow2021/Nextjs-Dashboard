a93fde35c3e0444e66e0dabda772c1bb560fedcf
HiLow <newbie_wisdom@hotmail.com>
2024-03-09 03:00:13 +0900

    update

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,10 @@
-import { Inter } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
 
+// Primary Font
 export const inter = Inter({ subsets: ['latin'] });
+
+// Secondary Font
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin']
+});
