cbba56ba083b66bd05474731e6b25f0f26ccc127
HiLow <newbie_wisdom@hotmail.com>
2024-03-12 01:44:24 +0900

    update

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -2,7 +2,7 @@ import { sql } from '@vercel/postgres';
 import { CustomerField, CustomersTableType, InvoiceForm, InvoicesTable, LatestInvoiceRaw, User, Revenue } from './definitions';
 import { formatCurrency } from './utils';
 
-import { PrismaClient, revenue } from '@prisma/client'
+import { PrismaClient, invoices, revenue } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export async function fetchRevenue() {
@@ -10,15 +10,7 @@ export async function fetchRevenue() {
     // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
     try {
-        // Artificially delay a response for demo purposes.
-        // Don't do this in production :)
-
-        // console.log('Fetching revenue data...');
-        // await new Promise((resolve) => setTimeout(resolve, 3000));
-
         const data = await prisma.$queryRaw<revenue[]>`SELECT * FROM revenue`;
-
-        // console.log('Data fetch completed after 3 seconds.');
 
         return data;
     } catch (error) {
@@ -29,17 +21,18 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
     try {
-        const data = await sql<LatestInvoiceRaw>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY invoices.date DESC
-      LIMIT 5`;
+        const data = await prisma.$queryRaw<LatestInvoiceRaw[]>`
+            SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
+            FROM invoices
+            JOIN customers ON invoices.customer_id = customers.id
+            ORDER BY invoices.date DESC
+            LIMIT 5`;
 
-        const latestInvoices = data.rows.map((invoice) => ({
+        const latestInvoices = data.map((invoice) => ({
             ...invoice,
             amount: formatCurrency(invoice.amount)
         }));
+        
         return latestInvoices;
     } catch (error) {
         console.error('Database Error:', error);
