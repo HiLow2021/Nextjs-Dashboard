928cf4261e0b3ea980bd746c828bfbf143c639fd
HiLow <newbie_wisdom@hotmail.com>
2024-03-14 17:02:35 +0900

    update

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -61,7 +61,11 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
+
+    throw new Error('Failed to Delete Invoice');
     try {
+        
+
         await prisma.$executeRaw`DELETE FROM invoices WHERE id = ${id}::uuid`;
         revalidatePath('/dashboard/invoices');
 
